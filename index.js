c54178661d91ab4ea8d2af7bc55aa01b553e3206
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-03-26 19:23:45 -0600

    added more js files to the library folder, added filename question for inquirer prompt, built skeleton for some functions

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateLogo = require("./lib/generateLogo");
 
 const questions = [
+  // Prompt user for name of logo to use as file name
+  {
+    type: "input",
+    name: "fileName",
+    message: "What would you like to name your file?",
+  },
   // Prompt user for up to three characters
   {
     type: "input",
     name: "text",
-    message: "Please enter up to three characters for the logo",
+    message: "Please enter up to three characters for the logo:",
   },
   // Prompt user for text color
   {
     type: "input",
     name: "textColor",
     message:
-      "Please enter a color or hexadecimal number for the logo's text color",
+      "Please enter a color or hexadecimal number for the logo's text color:",
   },
   // Prompt user for a shape
   {
     type: "list",
     name: "shape",
-    message: "Please choose one of the shapes below",
+    message: "Please choose one of the shapes below:",
+    choices: ["circle", "triangle", "square"],
   },
   // Prompt user for shape color
   {
     type: "input",
     name: "shapeColor",
-    message: "Please enter a color or hexadecimal number for the shape's color",
+    message:
+      "Please enter a color or hexadecimal number for the shape's color:",
   },
 ];
 
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log(answers);
+    const fileName = answers.fileName;
+    // Once prompt is completed, create SVG file in examples folder
+    // fs.writeFile(`./examples/${fileName}.svg`, generateLogo(answers));
   });
 }
 
